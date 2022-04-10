cfcb911c9671eace19c6c8d6b8be3254f453ed1d
TsotneGit <giorgadzetsotne1@gmail.com>
2022-04-10 16:56:27 +0400

    innitial commit

---
+++ website/views.py
@@ -0,0 +1,17 @@
+from flask import (
+    Blueprint,
+    redirect,
+    render_template,
+    request,
+    flash,
+    redirect,
+    url_for,
+)
+
+views = Blueprint("views", __name__)
+
+
+@views.route("/")
+@views.route("/home")
+def home():
+    return render_template("home.html")
